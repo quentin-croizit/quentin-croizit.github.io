2e338295568022f17c41ca554eac03e91c7261cc
EDJAntimatter <48672858+EDJAntimatter@users.noreply.github.com>
2020-05-22 10:53:36 +0200

    * changes
    * changes fullscreen method by detecting if the user is on mobile first
    * remove the function call in html because it's already called in the js file

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,9 +1,15 @@
 function resize()
  {
-    var heights = window.innerHeight;
-    var divs = document.getElementsByClassName("section");
-    for(var i=0;i<divs.length;i++)
-       divs[i].style.height = heights + "px";
+    if(isMobile){
+        var heights = window.innerHeight;
+        var divs = document.getElementsByClassName("section");
+        for(var i=0;i<divs.length;i++)
+           divs[i].style.height = heights + "px";
+    }
  }
+
+isMobile = window.matchMedia("(max-width: 840px)");
+
 resize();
+
 window.onresize = resize;
