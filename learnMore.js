3c61dd10fc6b6a2366f17cc8d6b9949c3892c83b
EDJAntimatter <48672858+EDJAntimatter@users.noreply.github.com>
2021-03-07 21:35:05 +0100

    Update learnMore.js

--- learnMore.js
+++ learnMore.js
@@ -12,18 +12,20 @@ for(var i = 0 ; i < cell.length ; i++) {
 }
 
 function onHover(){
+    if(screen.width >= 1280) {
     this.children[0].setAttribute('style', 'border-radius: 1.75rem');
     this.children[0].style.scale = 1.1;
     this.children[2].style.opacity = 1;
     this.children[2].style.scale = 1.05;
     this.style.background = "linear-gradient(to right bottom,#42669630,#658ec630)";
-}
+}}
 
 function onOut(){
+    if(screen.width >= 1280) {
     this.children[0].removeAttribute('style');
     this.children[0].style.scale = 1;
     this.children[2].style.opacity = 0.5;
     this.children[2].style.scale = 0.75;
     this.style.background = "linear-gradient( to right bottom,rgba(255, 255, 255, 0.4),rgba(255, 255, 255, 0.2))";
-}
+}}
 
