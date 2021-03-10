6194cf8b75d2119067d84fbc0da058d21e8d12d2
EDJAntimatter <48672858+EDJAntimatter@users.noreply.github.com>
2021-03-10 16:31:24 +0100

    modified learnMore.js to accomodate non-ff limitations

--- learnMore.js
+++ learnMore.js
@@ -5,27 +5,22 @@ console.log(cell.length);
 for(var i = 0 ; i < cell.length ; i++) {
     cell[i].addEventListener('mouseover',onHover);
     cell[i].addEventListener('mouseout',onOut);
-    cell[i].children[0].style.scale = 1;
-    cell[i].children[2].style.opacity = 0.5;
-    cell[i].children[2].style.scale = 0.75;
+    cell[i].children[0].setAttribute('style', 'transform: scale(1); border-radius: 1rem;');
+    cell[i].children[2].setAttribute('style', 'transform: scale(0.75); opacity: 0.5;');
     cell[i].style.background = "linear-gradient( to right bottom,rgba(255, 255, 255, 0.4),rgba(255, 255, 255, 0.2))";
 }
 
 function onHover(){
     if(screen.width >= 1280) {
-    this.children[0].setAttribute('style', 'border-radius: 1.75rem');
-    this.children[0].style.scale = 1.1;
-    this.children[2].style.opacity = 1;
-    this.children[2].style.scale = 1.05;
+    this.children[0].setAttribute('style', 'transform: scale(1.1); border-radius: 1.75rem;');
+    this.children[2].setAttribute('style', 'transform: scale(1.05); opacity: 1;');
     this.style.background = "linear-gradient(to right bottom,#42669630,#658ec630)";
 }}
 
 function onOut(){
     if(screen.width >= 1280) {
-    this.children[0].removeAttribute('style');
-    this.children[0].style.scale = 1;
-    this.children[2].style.opacity = 0.5;
-    this.children[2].style.scale = 0.75;
+    this.children[0].setAttribute('style', 'transform: scale(1); border-radius: 1rem;');
+    this.children[2].setAttribute('style', 'transform: scale(0.75); opacity: 0.5;');
     this.style.background = "linear-gradient( to right bottom,rgba(255, 255, 255, 0.4),rgba(255, 255, 255, 0.2))";
 }}
 
