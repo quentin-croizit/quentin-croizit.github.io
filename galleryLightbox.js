b901e3e2ab6111e9bbd2d62dff0e27b34632a79e
EDJAntimatter <quentin.croizit@gmail.com>
2021-03-21 13:48:34 +0100

    update lightbox to adapt for chrome/edge

--- galleryLightbox.js
+++ galleryLightbox.js
@@ -1,22 +1,34 @@
-var blocker = document.getElementsByClassName('lightbox-blocker')[0];
+var lightbox = document.getElementsByClassName('lightbox')[0];
+var lightbox_image = lightbox.children[0];
+var lightbox_blocker = lightbox.children[1];
 var images = document.getElementsByClassName('gallery-images')[0].children;
 
-blocker.addEventListener('click',blockerOnClick);
+lightbox_blocker.addEventListener('click',blockerOnClick);
 for(var i = 0 ; i < images.length ; i++) {
     images[i].addEventListener('click',imageOnClick);
 }
 
+console.log(lightbox, lightbox_image, lightbox_blocker);
+
 function imageOnClick(){
-    this.classList.add('lightbox');
-    blocker.classList.add('lightbox-cover');
+    console.log("click-image");
+
+    lightbox_image.src = this.src;
+    lightbox.classList.remove('deactivated');
+    lightbox_image.classList.remove('deactivated');
+    lightbox_blocker.classList.remove('deactivated');
+
     //Disables scrolling
     document.body.setAttribute('style', 'overflow: hidden;');
 }
 
 function blockerOnClick(){
     console.log("click-blocker");
-    document.getElementsByClassName('lightbox')[0].classList.remove('lightbox');
-    blocker.classList.remove('lightbox-cover');
+    
+    lightbox.classList.add('deactivated');
+    lightbox_image.classList.add('deactivated');
+    lightbox_blocker.classList.add('deactivated');
+
     //Re-enables scrolling
     document.body.setAttribute('style', 'overflow: auto;');
 }
