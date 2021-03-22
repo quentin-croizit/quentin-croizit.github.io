d36ec6b4acf781831d2b760e30b709ead96f9375
EDJAntimatter <quentin.croizit@gmail.com>
2021-03-22 13:35:40 +0100

    added tcia and beasts pages + added image cover if no trailer

--- galleryLightbox.js
+++ galleryLightbox.js
@@ -2,6 +2,15 @@ var lightbox = document.getElementsByClassName('lightbox')[0];
 var lightbox_image = lightbox.children[0];
 var lightbox_blocker = lightbox.children[1];
 var images = document.getElementsByClassName('gallery-images')[0].children;
+var cover = null;
+
+console.log(document.getElementsByClassName('image-cover').length);
+
+if (document.getElementsByClassName('image-cover').length>0) {
+    cover = document.getElementsByClassName('image-cover')[0];
+    cover.addEventListener('click',imageOnClick);
+};
+
 
 lightbox_blocker.addEventListener('click',blockerOnClick);
 for(var i = 0 ; i < images.length ; i++) {
@@ -9,6 +18,7 @@ for(var i = 0 ; i < images.length ; i++) {
 }
 
 console.log(lightbox, lightbox_image, lightbox_blocker);
+console.log(images)
 
 function imageOnClick(){
     console.log("click-image");
